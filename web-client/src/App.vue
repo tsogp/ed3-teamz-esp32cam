8630cc4578f11b0a658e622822ce23fdf323b4a2
Potemkin Pavel <potemkinpavel3@gmail.com>
2024-12-05 23:08:53 +0700

    add frontend

---
+++ web-client/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import CameraView from './views/camera-view/CameraView.vue'
+</script>
+
+<template>
+  <CameraView />
+</template>
---
+++ web-client/src/views/camera-view/CameraView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="relative w-full h-screen bg-gray-900">
+    <div v-if="hasError" class="absolute inset-0 flex flex-col items-center justify-center space-y-4 text-white">
+      <div class="animate-spin rounded-full h-32 w-32 border-t-4 border-blue-500"></div>
+      <p class="text-lg font-semibold">Error loading stream. Retrying...</p>
+    </div>
+    <img v-else :src="streamUrl" alt="MJPEG Stream" class="absolute inset-0 w-full h-full object-contain"
+      @error="handleStreamError" />
+
+    <div class="absolute inset-0 flex flex-row justify-between p-14 pointer-events-none">
+      <div class="self-end pointer-events-auto">
+        <Joystick :movement-type="JoystickMovementType.OMNIDIRECTIONAL" />
+      </div>
+      <div class="self-end pointer-events-auto">
+        <Joystick :movement-type="JoystickMovementType.ROTATIONAL" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Joystick from '../../components/joystick/Joystick.vue';
+import { JoystickMovementType } from '../../components/joystick/types';
+
+const streamUrl = ref("http://mecanum-car.local");
+const hasError = ref(false);
+
+const handleStreamError = () => {
+  console.error("Error loading the MJPEG stream.");
+  hasError.value = true;
+};
+</script>

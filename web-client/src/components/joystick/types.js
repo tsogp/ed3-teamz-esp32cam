b339928750de4c28965e3e75d085fe4b69b5afd7
Potemkin Pavel <specialforbtsu@gmail.com>
2024-12-06 15:46:49 +0700

    reduced joystick payload

--- web-client/src/components/joystick/types.js
+++ web-client/src/components/joystick/types.js
@@ -1,11 +1,16 @@
 const JoystickMovementType = Object.freeze({
-  OMNIDIRECTIONAL: 1,
-  ROTATIONAL: 2,
+  OMNIDIRECTIONAL: 0,
+  ROTATIONAL: 1,
 });
+
+const AVAILABLE_KEYS = [
+  ['w', 'a', 's', 'd'],
+  ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
+]
 
 const JOYSTICK_SIZE = 200;
 
 const BASE_COLOR = "black";
 const STICK_COLOR = "purple"; 
 
-export { JoystickMovementType, JOYSTICK_SIZE, BASE_COLOR, STICK_COLOR };
+export { JoystickMovementType, JOYSTICK_SIZE, BASE_COLOR, STICK_COLOR, AVAILABLE_KEYS };

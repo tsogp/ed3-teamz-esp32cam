73011cbb544675275fc5056ade5f0e44282b0d9c
Potemkin Pavel <potemkinpavel3@gmail.com>
2024-12-06 14:42:43 +0700

    undefined joystick callbacks fixed

--- web-client/src/components/joystick/Joystick.vue
+++ web-client/src/components/joystick/Joystick.vue
@@ -1,8 +1,8 @@
 <template>
   <Joystick ref="joystick" :size="JOYSTICK_SIZE" :stick-size="JOYSTICK_SIZE / 2" :base-color="BASE_COLOR"
     :stick-color="STICK_COLOR" :throttle="100"
-    @stop="$emit('movementChanged', { x: event?.x, y: event?.y })"
-    @move="$emit('movementEnded', { x: event?.x, y: event?.y })" />
+    @stop="$emit('movementChanged', { x: $event?.x ?? 0, y: $event?.y ?? 0 })"
+    @move="$emit('movementEnded', { x: $event?.x ?? 0, y: $event?.y ?? 0 })" />
 </template>
 
 <script setup>

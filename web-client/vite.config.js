e1e7462eca0dc561fffe91d41665d7977164a74a
Potemkin Pavel <potemkinpavel3@gmail.com>
2024-12-08 10:22:34 +0700

    added tailwind for autoprefixing

--- web-client/vite.config.js
+++ web-client/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue()],
 })

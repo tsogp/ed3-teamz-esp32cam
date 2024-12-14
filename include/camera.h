0c8666cf1c2c5b79408b0ea22d1e8431a73a2835
Potemkin Pavel <potemkinpavel3@gmail.com>
2024-12-14 12:22:40 +0700

    refactor camera module

--- include/camera.h
+++ include/camera.h
@@ -3,30 +3,9 @@
 
 #include "esp_camera.h"
 #include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-#define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1
-#define CAM_PIN_XCLK 0
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 21
-#define CAM_PIN_D2 19
-#define CAM_PIN_D1 18
-#define CAM_PIN_D0 5
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
-
-#define CONFIG_XCLK_FREQ 20000000
 
 esp_err_t init_camera();
-esp_err_t reinit_camera(framesize_t frame_size);
+int change_camera_resolution(framesize_t frame_size);
+int change_camera_jpeg_quality(int quality);
 
 #endif // __CAMERA_H__

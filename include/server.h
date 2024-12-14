f318de42642f89fb324068d6842efcac79e660cb
Potemkin Pavel <specialforbtsu@gmail.com>
2024-12-14 13:42:06 +0700

    refactor jpg streaming handler

--- include/server.h
+++ include/server.h
@@ -1,14 +1,15 @@
 #ifndef __SERVER_H__
 #define __SERVER_H__
 
-#include "esp_http_server.h"
-#include "esp_camera.h"
-#include "esp_timer.h"
-#include "esp_log.h"
+#include <esp_http_server.h>
+#include <esp_camera.h>
+#include <esp_timer.h>
+#include <esp_log.h>
+#include <cJSON.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
 #include "camera.h"
-#include "cJSON.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/semphr.h"
+#include "server/camera_handlers.h"
 
 void start_webserver(void);
 void stop_webserver(void);

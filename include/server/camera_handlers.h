8a79d80c6bf27cd02d4bb7f1c24a8f48cc7f513d
Potemkin Pavel <specialforbtsu@gmail.com>
2024-12-15 21:55:50 +0700

    remove unnecessary logic

--- include/server/camera_handlers.h
+++ include/server/camera_handlers.h
@@ -7,9 +7,13 @@
 #include <esp_http_server.h>
 #include <esp_camera.h>
 #include <cJSON.h>
+
 #include "camera.h"
+#include "server/ws_handlers.h"
 
 esp_err_t stream_jpg(httpd_req_t *req);
-esp_err_t reload_camera_config(httpd_req_t *req);
+esp_err_t reload_camera_resolution(httpd_req_t *req);
+esp_err_t reload_camera_quality(httpd_req_t *req);
+esp_err_t toggle_flash_handler(httpd_req_t *req);
 
 #endif // __CAMERA_HANDLERS_H__

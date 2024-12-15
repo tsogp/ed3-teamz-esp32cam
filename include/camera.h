522f0b92b9341f9bd3b85bcc0120ce80c7287fc6
Potemkin Pavel <potemkinpavel3@gmail.com>
2024-12-15 21:55:30 +0700

    add flash pin and change .fb_count to 2

--- include/camera.h
+++ include/camera.h
@@ -3,9 +3,11 @@
 
 #include "esp_camera.h"
 #include "esp_log.h"
+#include <freertos/FreeRTOS.h>
 
 esp_err_t init_camera();
 int change_camera_resolution(framesize_t frame_size);
 int change_camera_jpeg_quality(int quality);
+esp_err_t toggle_camera_flash();
 
 #endif // __CAMERA_H__

9abc8beca690ebbf382352a95ddb43b8b3d0c7ef
Potemkin Pavel <specialforbtsu@gmail.com>
2024-12-17 16:30:43 +0700

    add button component

---
+++ web-client/src/components/button/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    class="inline-flex justify-center items-center cursor-pointer border-2 border-black text-black hover:bg-slate-50 rounded-lg p-2"
+    @click="handleClick">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits();
+
+const handleClick = () => {
+  emit('click');
+};
+</script>

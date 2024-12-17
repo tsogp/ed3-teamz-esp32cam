43ce5fdc02061d3ce293824630de252006183c4a
Potemkin Pavel <specialforbtsu@gmail.com>
2024-12-17 16:30:50 +0700

    add button with icon

---
+++ web-client/src/components/icon-button/IconButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <button 
+    class="inline-flex justify-center items-center cursor-pointer border-2 border-black hover:bg-slate-50 rounded-lg p-2" 
+    @click="handleClick">
+    <img :src="icon" :alt="altText" class="h-full w-full" :style="iconStyles" />
+  </button>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+// Define the props
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  altText: {
+    type: String,
+    default: 'Icon',
+  },
+  width: {
+    type: String,
+    default: '24px',
+  },
+  height: {
+    type: String,
+    default: '24px',
+  },
+  iconTransform: {
+    type: String,
+    default: '',
+  }
+});
+
+// Emit click event
+const emit = defineEmits();
+
+const handleClick = () => {
+  emit('click');
+};
+
+// Computed property for icon styles
+const iconStyles = computed(() => {
+  const styles = {
+    width: props.width,
+    height: props.height,
+  };
+
+  if (props.iconTransform) {
+    styles.transform = props.iconTransform;
+  }
+
+  return styles;
+});
+</script>

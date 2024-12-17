b6ffc0c3a131c8bc613fe47a9b32a87124ea6f3e
Potemkin Pavel <specialforbtsu@gmail.com>
2024-12-17 16:31:38 +0700

    ui testing

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -4,6 +4,9 @@ import Dropdown from './components/dropdown/Dropdown.vue';
 import { AVAILABLE_RESOLUTIONS, AVAILABLE_JPEG_QUALITY } from './types';
 import api from './services/generic-api';
 import { onMounted } from 'vue';
+import Slider from './components/slider/Slider.vue';
+import IconButton from './components/icon-button/IconButton.vue';
+import Button from './components/button/Button.vue';
 
 const changeResolution = async (resId) => {
   try {
@@ -43,5 +46,23 @@ onMounted(async () => {
         @select="(event) => changeQuality(event)" />
     </div>
     <CameraView class="" />
+    <Slider :min="0" :max="3" :initial-value="0" name="Throttle" />
+    <div class="grid grid-cols-3 grid-rows-3 gap-4 w-52 h-52">
+      <IconButton icon="arrow.svg" altText="Example Icon" icon-transform="rotate(-45deg)" />
+      <IconButton icon="arrow.svg" altText="Example Icon" />
+      <IconButton icon="arrow.svg" altText="Example Icon" icon-transform="rotate(45deg)" />
+      <IconButton icon="arrow.svg" altText="Example Icon" icon-transform="rotate(-90deg)" />
+      <IconButton icon="arrow.svg" altText="Example Icon" icon-transform="rotate(90deg)"
+        class="col-start-3 col-end-4" />
+      <IconButton icon="arrow.svg" altText="Example Icon" icon-transform="rotate(-135deg)" />
+      <IconButton icon="arrow.svg" altText="Example Icon" icon-transform="rotate(180deg)" />
+      <IconButton icon="arrow.svg" altText="Example Icon" icon-transform="rotate(135deg)" />
+    </div>
+    <div class="flex flex-col gap-y-3">
+      <Button text="Square with turn" />
+      <Button text="Circle with turn" />
+      <Button text="Square no turn" />
+      <Button text="Circle drift" />
+    </div>
   </div>
 </template>

7abd8b92fc23607002a61a3332d850e0e82d521c
Potemkin Pavel <specialforbtsu@gmail.com>
2025-01-05 22:44:54 +0700

    attempt to implement ping pong in websokcet

--- src/server.c
+++ src/server.c
@@ -1,4 +1,5 @@
 #include "server.h"
+#include <server/keep_alive.h>
 
 #define TAG "SERVER"
 
@@ -17,6 +18,11 @@ typedef struct {
 	httpd_req_t *req;
 	httpd_req_handler_t handler;
 } httpd_async_req_t;
+
+struct async_resp_arg {
+	httpd_handle_t hd;
+	int fd;
+};
 
 static esp_err_t queue_request(httpd_req_t *req, httpd_req_handler_t handler) {
 	ESP_LOGI(TAG, "got a queue request");
@@ -129,10 +135,64 @@ esp_err_t options_handler(httpd_req_t *req) {
 	return ESP_OK;
 }
 
-void start_webserver(void) {
-	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+static void send_ping(void *arg) {
+	struct async_resp_arg *resp_arg = arg;
+	httpd_handle_t hd = resp_arg->hd;
+	int fd = resp_arg->fd;
+	httpd_ws_frame_t ws_pkt;
+	memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+	ws_pkt.payload = NULL;
+	ws_pkt.len = 0;
+	ws_pkt.type = HTTPD_WS_TYPE_PING;
 
+	httpd_ws_send_frame_async(hd, fd, &ws_pkt);
+	free(resp_arg);
+}
+
+esp_err_t wss_open_fd(httpd_handle_t hd, int sockfd) {
+	ESP_LOGI(TAG, "New client connected %d", sockfd);
+	wss_keep_alive_t h = httpd_get_global_user_ctx(hd);
+	return wss_keep_alive_add_client(h, sockfd);
+}
+
+void wss_close_fd(httpd_handle_t hd, int sockfd) {
+	ESP_LOGI(TAG, "Client disconnected %d", sockfd);
+	wss_keep_alive_t h = httpd_get_global_user_ctx(hd);
+	wss_keep_alive_remove_client(h, sockfd);
+	close(sockfd);
+}
+
+bool client_not_alive_cb(wss_keep_alive_t h, int fd) {
+	ESP_LOGE(TAG, "Client not alive, closing fd %d", fd);
+	httpd_sess_trigger_close(wss_keep_alive_get_user_ctx(h), fd);
+	return true;
+}
+
+bool check_client_alive_cb(wss_keep_alive_t h, int fd) {
+	ESP_LOGD(TAG, "Checking if client (fd=%d) is alive", fd);
+	struct async_resp_arg *resp_arg = malloc(sizeof(struct async_resp_arg));
+	assert(resp_arg != NULL && "here failed");
+	resp_arg->hd = wss_keep_alive_get_user_ctx(h);
+	resp_arg->fd = fd;
+
+	if (httpd_queue_work(resp_arg->hd, send_ping, resp_arg) == ESP_OK) {
+		return true;
+	}
+	return false;
+}
+
+void start_webserver(void) {
+	wss_keep_alive_config_t keep_alive_config = KEEP_ALIVE_CONFIG_DEFAULT();
+	keep_alive_config.max_clients = 2;
+	keep_alive_config.client_not_alive_cb = client_not_alive_cb;
+	keep_alive_config.check_client_alive_cb = check_client_alive_cb;
+	wss_keep_alive_t keep_alive = wss_keep_alive_start(&keep_alive_config);
+
+	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 	config.max_open_sockets = CONFIG_EXAMPLE_MAX_ASYNC_REQUESTS + 1;
+	config.global_user_ctx = keep_alive;
+	config.open_fn = wss_open_fd;
+	config.close_fn = wss_close_fd;
 
 	static httpd_uri_t video_streamer = { .uri = "/",
 					      .method = HTTP_GET,

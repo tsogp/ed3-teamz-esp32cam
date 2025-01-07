467012889be0a543330cb27886b6a13f259f583f
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-01-07 21:05:23 +0700

    ws changes

--- include/server/ws_handlers.h
+++ include/server/ws_handlers.h
@@ -9,5 +9,6 @@
 
 esp_err_t controls_data_handler(httpd_req_t *req);
 esp_err_t trigger_async_send(double framerate);
+esp_err_t ws_disconnection_handler(httpd_handle_t hd, httpd_err_code_t err);
 
 #endif // __WS_HANDLERS_H__
